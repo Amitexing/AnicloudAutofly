c15dc30deb5040e787462b06734a2bbc2c1010a0
Amitexing <amitexing@gmail.com>
2023-11-27 23:27:43 +0300

    Убраны сообщения о включении fly.

---
+++ src/main/java/ru/anicloud/anicloudautofly/AnicloudAutofly.java
@@ -0,0 +1,26 @@
+package ru.anicloud.anicloudautofly;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AnicloudAutofly extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (player.hasPermission("anicloud.autofly")){
+            player.setAllowFlight(true);
+            player.setFlying(true);
+        }
+    }
+}
